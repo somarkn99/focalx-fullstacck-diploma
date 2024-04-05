a40115f914572479da6b61655cd70448eb8c6294
Somar Kesen <kesen.somar.99@gmail.com>
2024-04-06 00:24:01 +0300

    many to many

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Author;
+use Illuminate\Http\Request;
+
+class AuthorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $author = Author::create();
+        $author->books()->attach($request->book_id);
+        $author->books()->detach($request->book_id);
+        $author->books()->sync($request->book_id);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Author $author)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Author $author)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Author $author)
+    {
+        //
+    }
+}
