e8adec3bfd2735c31b0d69875336b4fcf4ed600e
Somar Kesen <kesen.somar.99@gmail.com>
2024-04-26 16:35:30 +0300

    factory & Seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,10 +24,10 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'first_name' => $this->faker->firstName,
+            'last_name' => $this->faker->lastName,
+            'email' => $this->faker->unique()->safeEmail,
+            'password' => bcrypt('password'),
             'remember_token' => Str::random(10),
         ];
     }
